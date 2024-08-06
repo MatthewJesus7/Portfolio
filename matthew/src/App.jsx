fd70cf458e65ca4c00e1812e40ea71130576f1fa
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-08-06 18:17:23 -0300

    Setup concluido

---
+++ matthew/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+
+      <h1 className="text-3xl font-bold underline text-white bg-black ">
+      Hello world!
+    </h1>
+      <p>Portfólio</p>
+
+    </div>
+  );
+}
+
+export default App;
