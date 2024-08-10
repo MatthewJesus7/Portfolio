bc8be4dc49fedd5f3af5353bc4a444210cc132fd
Matheus Costa Jesus <154378662+MatthewJesus7@users.noreply.github.com>
2024-08-10 17:05:23 -0300

    Colocando caminho absoluto para a imagens

--- matthew/src/Components/Sections/MainProjects.jsx
+++ matthew/src/Components/Sections/MainProjects.jsx
@@ -13,7 +13,7 @@ function MainProjects() {
         type="picture"
         // proof=""
         picture="true"
-        backgroundImage="url('/imagens/CommerceTech.png')"
+        backgroundImage="url('https://Portfolio/matthew.vercel.app/imagens/CommerceTech.png')"
         title="CommerceTech"
         text={`Desenvolvi o CommerceTech como uma demonstração das minhas habilidades e uma fonte de renda extra, com o objetivo de lançar um projeto completo e funcional. Enfrentei desafios durante o lançamento, mas aprendi que "feito é melhor que perfeito". O uso de React, Tailwind, e GitHub tornou o projeto robusto e eficiente, e essa experiência acelerou meu desenvolvimento como desenvolvedor.`}
         >
@@ -25,7 +25,7 @@ function MainProjects() {
         type="picture"
         // proof=""
         picture="true"
-        backgroundImage="url('/imagens/PathOfSucessEditada.png')"
+        backgroundImage="url('https://Portfolio/matthew.vercel.app/imagens/PathOfSucessEditada.png')"
         title="PathOfSucess"
         text="O objetivo desse projeto foi obter experiência e proficiência em HTML, CSS, JavaScript e Web Design, além de avaliar minha prontidão para o mercado de trabalho. Desenvolvi um site leve e rápido, enfrentando o desafio de criar meu primeiro carrossel em JavaScript. O maior destaque foi ter realizado todo o processo sozinho, desde o design até a implementação, provando que consigo transformar minhas ideias em realidade."
         >
