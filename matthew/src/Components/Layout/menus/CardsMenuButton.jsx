6f43514472ddf5a1fcd10bda0135d794c2a91698
Matheus Costa Jesus <mc5349063@gmail.com>
2024-12-14 22:00:52 -0300

    mudanças nos cards pré multi menus height

--- matthew/src/Components/Layout/menus/CardsMenuButton.jsx
+++ matthew/src/Components/Layout/menus/CardsMenuButton.jsx
@@ -33,14 +33,14 @@ const CardsMenuButton = ({ text, onClick }) => {
     return(
         <button onClick={handleOnClick}
         onMouseOver={handleMouseOver} 
-        className="flex w-[376px] rounded-md transition-all duration-300 ease-in-out -ml-5 px-5" >
+        className="flex w-full rounded-md transition-all duration-300 ease-in-out " >
             <h3>{text}</h3>
             {/* container do item que gira */}
             <div 
-            className={`flex justify-end absolute w-[376px] hover:-translate-y-1 h-7 hover:h-9 transition-all duration-500 ease-in-out 
+            className={`flex justify-end absolute w-[90%] hover:-translate-y-1 h-7 hover:h-9 transition-all duration-500 ease-in-out 
             `}>
             {/* item que gira */}
-            <p className={`font-semibold mr-5 size-5 transition-all duration-500 ease-in-out text-gray-500  
+            <p className={`font-semibold size-5 transition-all duration-500 ease-in-out text-gray-500  
             ${ menuOpen && "rotate-180 origin-center"}`}>
 
             {menuOpen 
@@ -56,7 +56,7 @@ const CardsMenuButton = ({ text, onClick }) => {
                 bg-gradient-to-r from-gray-700/50 to-transparent
                 transition-all duration-500 ease-out
             ${mouseOver
-            ? 'w-[394px]'
+            ? 'w-3/4'
             : 'w-0'
             }
             `}>
--- matthew/src/Components/Sections/Footer.jsx
+++ matthew/src/Components/Sections/Footer.jsx
@@ -1,20 +1,36 @@
-import Section from "../Layout/Section"
+import Section from "../Layout/Section";
+import { useLocation } from "react-router-dom"; // Importa o useLocation
 
 function Footer() {
-    return(
-        <footer>
-        <Section customclass="xl:ml-[40%] xl:w-[60%] relative z-10">
-            <p className="text-center">
-                Versão 1.0 do meu Portfólio. <br />
-                Design inspirado de:
-                <a href="https://brittanychiang.com/#projects" className="px-1.5 text-gray-900 hover:underline">
-                Portfolio de BrittanyChiang
-                </a> <br />
-                Feito com React e Tailwind.css
-            </p>
-        </Section>
-      </footer>
-    )
+  const location = useLocation();
+
+  const isIntroductionPage = location.pathname === "/";
+
+  return (
+    <footer 
+    className="w-full">
+      <Section
+        customclass={`relative z-10 ${
+          !isIntroductionPage 
+          ? "w-full" 
+          : "xl:ml-[40%] xl:w-[60%]"
+        }`}
+      >
+        <p className="text-center">
+          Versão 1.0 do meu Portfólio. <br />
+          Design inspirado de:
+          <a
+            href="https://brittanychiang.com/#projects"
+            className="px-1.5 text-gray-900 hover:underline"
+          >
+            Portfolio de BrittanyChiang
+          </a>
+          <br />
+          Feito com React e Tailwind.css
+        </p>
+      </Section>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
